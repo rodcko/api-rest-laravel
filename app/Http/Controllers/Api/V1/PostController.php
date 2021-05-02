077a434ee950807507ee24fd613a948a460a6a0f
Rodrigo Hernández <42217144+rodcko@users.noreply.github.com>
2021-05-01 19:26:53 -0600

    Version 1: delete method

--- app/Http/Controllers/Api/V1/PostController.php
+++ app/Http/Controllers/Api/V1/PostController.php
@@ -62,6 +62,10 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
+
+        return response()->json([
+            'message' => 'Success'
+        ], 204);
     }
 }
