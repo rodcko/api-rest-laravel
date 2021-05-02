6602b0e523801cc4a889028e75ef96a18ae9fb5d
Rodrigo Hernández <42217144+rodcko@users.noreply.github.com>
2021-05-01 21:05:07 -0600

    Version 2: collection

--- app/Http/Controllers/Api/V2/PostController.php
+++ app/Http/Controllers/Api/V2/PostController.php
@@ -7,7 +7,7 @@ use App\Models\Post;
 use Illuminate\Http\Request;
 
 use App\Http\Resources\V2\PostResource;
-use App\Http\Resources\V2\PostcCollection;
+use App\Http\Resources\V2\PostCollection;
 
 class PostController extends Controller
 {
@@ -18,7 +18,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        return new PostCollection(Post::latest()->paginate());
     }
 
     /**
