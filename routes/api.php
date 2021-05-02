7dbc3e619cd8d2248b58583f857212c3e203c6a7
Rodrigo Hernández <42217144+rodcko@users.noreply.github.com>
2021-05-01 21:15:13 -0600

    Private api

--- routes/api.php
+++ routes/api.php
@@ -8,8 +8,10 @@ use App\Http\Controllers\Api\V2\PostController as PostV2;
 
 // V1
 Route::apiResource('v1/posts', PostV1::class)
-  ->only(['index', 'show', 'destroy']);
+    ->only(['index', 'show', 'destroy'])
+    ->middleware('auth:sanctum');
 
 // V2
   Route::apiResource('v2/posts', PostV2::class)
-  ->only(['index', 'show', 'destroy']);
+    ->only(['index', 'show', 'destroy'])
+    ->middleware('auth:sanctum');
